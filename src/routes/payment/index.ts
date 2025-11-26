c937755ebf7572b7d00a42ef3c7bb29004e73b1e
Giorgi <g.margiani98@gmail.com>
2025-11-26 21:56:42 +0400

    add auth middleware *in front of* payment routes

--- src/routes/payment/index.ts
+++ src/routes/payment/index.ts
@@ -1,8 +1,11 @@
 import type { FastifyInstance } from 'fastify';
 import { depositSchema, transferSchema } from './schemas';
 import { depositHandler, transferHandler } from './handler';
+import { authenticate } from '../../middleware/auth';
 
 export default async function paymentRoutes(fastify: FastifyInstance) {
+  fastify.addHook('preHandler', authenticate);
+
   fastify.post('/deposit', { schema: depositSchema }, depositHandler);
 
   fastify.post('/transfer', { schema: transferSchema }, transferHandler);
